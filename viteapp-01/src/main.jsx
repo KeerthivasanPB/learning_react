ab465a8239d7004735e2d37a0cdf617692da1a4e
P B Keerthivasan <keerthivasanpb.codesforfun@gmail.com>
2025-08-01 22:56:43 +0530

    create custom react

--- viteapp-01/src/main.jsx
+++ viteapp-01/src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Youtube from "./Youtube.jsx";
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-    <Youtube />
-  </>
+import React from "react";
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://www.google.com", target: "_blank" },
+  "visit google by clicking me"
 );
+
+createRoot(document.getElementById("root")).render(reactElement);
