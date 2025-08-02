29bfe52e3043d29dbfac3e0e726b80b94d44f663
P B Keerthivasan <keerthivasanpb.codesforfun@gmail.com>
2025-08-03 01:43:47 +0530

    add responsive card component by using props

--- tailwind-props-03/src/main.jsx
+++ tailwind-props-03/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Card from "./components/card.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <Card username="Keerthivasan" role="web dev" />
+  </>
+);
